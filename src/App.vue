458844a97abb5210dbddfbe3ddd9153cc484a30e
AngeloCalabro <angelo.1991@hotmail.com>
2022-11-10 11:50:38 +0100

    component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<HeaderComponent/>
+<main>
+<ProductList/>
+</main>
+<FooterComponent/>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import ProductList from './components/ProductList.vue';
+import FooterComponent from './components/FooterComponent.vue';
+  export default {
+    components: { HeaderComponent, ProductList, FooterComponent }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        Sono lista prodotti
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'ProductList'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
